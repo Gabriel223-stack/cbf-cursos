4845bed20d440a00ee5e8e78907a1cdb109a676b
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-04-22 20:54:35 -0300

    Operador ternário

---
+++ aulas/aula09/aula9.js
@@ -0,0 +1,22 @@
+/* SIMPLIFICANDO CONDIÇÕES COM OPERADOR TERNÁRIO */
+
+let st = "I"
+
+res = (st == "A" ? "Ativo" : "Inativo")
+
+console.log(res)
+//let num1 = 10
+//let num2 = 5
+//res = (!(num % 2) ? 'Par' : 'Impar')
+//res = (num1 > num2 ? "Verdadeiro" : "Falso")
+
+// TESTE LÓGICO ? SE VERDAEIRO : SE FALSO
+
+// let res = num % 2
+
+/* if (res == 0) {
+    console.log('Par')
+} else {
+    console.log('Impar')
+}
+*/
