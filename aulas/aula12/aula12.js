92bc75a7d7e8c20d130165748ccc6afaedaaaa69
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-04-26 19:47:07 -0300

    Condicional IF e ELSE

---
+++ aulas/aula12/aula12.js
@@ -0,0 +1,35 @@
+let energia = 100
+let clima = 'chovendo'
+/* 
+if -> se 
+else -> se não ou caso contrário
+*/
+
+if (energia > 70 || clima == 'sol') {
+    console.log('Vou a praia')
+} else {
+    console.log('Vou ao cinema')
+}
+
+// EXEMPLO DE IF E ELSE
+/*
+if (num > 10) {
+    console.log('Numeral maior que 10')
+    if (num > 50) {
+        console.log('Numeral maior que 50')
+    }
+} else if (num > 5) {
+    console.log('Numeral esta entre 6 e 10')
+} else {
+    console.log('Numeral menor ou igual a 5')
+}
+console.log('Fim do programa')
+*/
+
+/* ANINHAMENTO 
+IF {
+    IF {
+    
+    }    
+}
+*/
