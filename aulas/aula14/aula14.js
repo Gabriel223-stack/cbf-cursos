17673ae6093a9f46f91b1b30ea9af68f3ee9ae11
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-04-30 20:28:35 -0300

    loop for

---
+++ aulas/aula14/aula14.js
@@ -0,0 +1,13 @@
+console.log('Início do programa')
+for (let i = 0; i < 100; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} o valor é par`)
+    } else {
+        console.log(`${i} o valor é impar`)
+    }
+}
+console.log('Fim do programa')
+
+/*
+0 - 1 - 2 - 3 até 10 *LAÇO DE REPETIÇÃO
+*/
