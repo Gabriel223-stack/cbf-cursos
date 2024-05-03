7f3cf279961b1be8f7824dba32d776785aef3dfb
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-05-03 20:11:56 -0300

    diferença entre while e do while

---
+++ aulas/aula17/aula17.js
@@ -0,0 +1,16 @@
+let n = 10
+
+do {
+    console.log('CBF Cursos')
+    n++
+} while (n < 10) {
+    console.log('Fim do programa')
+}
+
+/*
+while (n < 10) {
+    console.log('CBF Cursos')
+    n++
+}
+console.log('Fim do programa')
+*/
