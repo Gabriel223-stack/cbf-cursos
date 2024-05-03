8a21e7738ebb3921c060260141b24455480e0786
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-05-03 20:11:15 -0300

    while

---
+++ aulas/aula16/aula16.js
@@ -0,0 +1,24 @@
+// FATORIAL 5! = 5*4*3*2*1 = 120
+
+/* CRESCENTE
+let n = 1
+let fat = 10
+
+while (n <= 10) {
+    fat = fat * n
+    n++
+}
+
+console.log(fat)
+*/
+
+// DECRESCENTE
+let n = 5
+let fat = 1
+
+while (n >= 1) {
+    fat *= n
+    n--
+}
+
+console.log(fat)
