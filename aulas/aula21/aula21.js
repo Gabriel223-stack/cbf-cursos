270f1b54ac94b339a9d4fc6b2ace01c5ba75c903
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-05-09 19:55:33 -0300

    funções parametrizadas

---
+++ aulas/aula21/aula21.js
@@ -0,0 +1,47 @@
+/* function soma(n1, n2) {
+     console.log(n1+n2)
+}
+
+soma(10, 5)
+*/
+
+// UM SO VALOR, DAI COLOCA NOS PARAMETROS O QUE VÃO RECEBER N1=1, N2=2
+
+/*
+const valor_padrao = 0
+function soma(n1 = valor_padrao, n2 = valor_padrao) {
+    //console.log(n1+n2)
+    let res
+    res = n1+n2
+    return res
+}
+
+let resultado_soma = soma(5)
+console.log(resultado_soma)
+*/
+
+
+function add(v) {
+    return valor + v
+    // OU valor += v
+}
+
+let valor = 0
+console.log(valor)
+
+valor = add(10)
+console.log(valor)
+
+valor = add(5)
+console.log(valor)
+
+/* OU  
+let valor = 0
+console.log(valor)
+
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)
+*/
