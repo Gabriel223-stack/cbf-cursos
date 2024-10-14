721a13519c0202a0e683b46f28d9b6e4c3b33d92
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-10-14 20:55:10 -0300

    Criando novos elementos e adicionando no DOM

---
+++ aulas/Criando elementos e adicionando no DOM/aula39.js
@@ -0,0 +1,12 @@
+const caixa1 = document.querySelector('#caixa1')
+const btn_c = [...document.querySelectorAll('.curso')]
+const c1_2 = document.querySelector('#c1_2')
+const cursos = ["HTML", "CSS", "JavaScript", "PHP", "React", "MySQL", "ReactNative"]
+
+cursos.map((el, chave) => {
+    const novoElemento = document.createElement("div") //CRIANDO O ELEMENTO
+    novoElemento.setAttribute("id", `c${chave}`) // INSERINDO ID
+    novoElemento.setAttribute("class","curso c1") // INSERINDO CLASS
+    novoElemento.innerHTML = el
+    caixa1.appendChild(novoElemento) // CHAMANDO O ELEMENTO CRIADO
+})
